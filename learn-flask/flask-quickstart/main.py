72053e623e2338655528ab0a239f206795549684
xihuyu20000 <xihuyu20000@users.noreply.github.com>
2023-10-04 11:05:18 +0800

    调试模式

--- learn-flask/flask-quickstart/main.py
+++ learn-flask/flask-quickstart/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-print('模板的搜索路径',app.jinja_loader.searchpath)
 @app.route('/')
 def index():
     return render_template('index.html')
