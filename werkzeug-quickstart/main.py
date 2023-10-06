5d97be049d8bfa5d1d7f8c2588031c7ec60644f1
xihuyu20000 <xihuyu20000@users.noreply.github.com>
2023-10-06 10:35:56 +0800

    返回文本和html

--- werkzeug-quickstart/main.py
+++ werkzeug-quickstart/main.py
@@ -3,9 +3,9 @@ from werkzeug import run_simple, Response
 
 class Application:
     def __call__(self, env, start_response):
-        resp = Response()
+        text = '<strong>hello world</strong>'
+        resp = Response(text, mimetype='text/html')
         return resp(env, start_response)
 
-# app是一个callable对象
 app = Application()
 run_simple('0.0.0.0', 5000, app)
