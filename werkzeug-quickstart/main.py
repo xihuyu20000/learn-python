1122a30c4f14ff7a2b77d54567b0254bb21de789
xihuyu20000 <xihuyu20000@users.noreply.github.com>
2023-10-06 10:46:18 +0800

    路由

--- werkzeug-quickstart/main.py
+++ werkzeug-quickstart/main.py
@@ -1,9 +1,22 @@
-from werkzeug import run_simple, Response
+from collections import defaultdict
 
+import jinja2
+from werkzeug import run_simple, Response, Request
+
+url_map = defaultdict(str)
+url_map['/'] = 'index.html'
+url_map['/user'] = 'user.html'
 
 class Application:
+    def __init__(self):
+        self.jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), autoescape=True)
+        print('模板', self.jinja_env.loader.list_templates())
+
     def __call__(self, env, start_response):
-        text = '<strong>hello world</strong>'
+        req = Request(env)
+        print('请求路径', req.path)
+        template_name = url_map.get(req.path, 'index.html')
+        text = self.jinja_env.get_template(template_name).render()
         resp = Response(text, mimetype='text/html')
         return resp(env, start_response)
 
