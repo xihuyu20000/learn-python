8990d90aef95d95c6b10698c5bf04de9584041eb
xihuyu20000 <xihuyu20000@users.noreply.github.com>
2023-12-19 10:05:00 +0800

    echart封装组件

--- SciTools/graphics/src/App.vue
+++ SciTools/graphics/src/App.vue
@@ -1,32 +1,8 @@
 <template>
-  <div ref="chart1" style="width: 400px; height: 400px"></div>
+  <base-echart></base-echart>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
-import * as echarts from "echarts";
-
-const chart1 = ref(null); // 创建dom引用
-
-onMounted(() => {
-  const myChart = echarts.init(chart1.value); // 初始化echarts实例
-  const option = {
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [150, 230, 224, 218, 135, 147, 260],
-        type: "line",
-      },
-    ],
-  };
-
-  myChart.setOption(option);
-});
+import BaseEchart from "@/components/BaseEchart.vue";
 </script>
 
 <style lang="scss" scoped>
---
+++ SciTools/graphics/src/components/BaseEchart.vue
@@ -0,0 +1,34 @@
+<!-- echarts组件 -->
+<template>
+  <div ref="chart1" style="width: 400px; height: 400px"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import * as echarts from "echarts";
+
+const chart1 = ref(null); // 创建dom引用
+
+onMounted(() => {
+  const myChart = echarts.init(chart1.value); // 初始化echarts实例
+  const option = {
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: "line",
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+});
+</script>
+<style lang='scss' scoped>
+</style>
