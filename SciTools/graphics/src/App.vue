5ff1a6693311e2d2ae76ff0fd72299edfb642815
xihuyu20000 <xihuyu20000@users.noreply.github.com>
2023-12-19 10:44:45 +0800

    echarts组件，根据窗口自适应变化

--- SciTools/graphics/src/App.vue
+++ SciTools/graphics/src/App.vue
@@ -3,6 +3,9 @@
 </template>
 <script setup>
 import BaseEchart from "@/components/BaseEchart.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {});
 </script>
 
 <style lang="scss" scoped>
--- SciTools/graphics/src/components/BaseEchart.vue
+++ SciTools/graphics/src/components/BaseEchart.vue
@@ -1,6 +1,6 @@
 <!-- echarts组件 -->
 <template>
-  <div ref="chart1" style="width: 400px; height: 400px"></div>
+  <div ref="chart1" style="width: 100%; height: 100%"></div>
 </template>
 
 <script setup>
@@ -9,6 +9,21 @@ import * as echarts from "echarts";
 
 const chart1 = ref(null); // 创建dom引用
 
+// 防抖
+const throttle = function (fn, wait) {
+  let timer = null;
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (!timer) {
+      // 当延迟时间结束后，执行函数
+      timer = setTimeout(() => {
+        timer = null;
+        fn.apply(context, args);
+      }, wait);
+    }
+  };
+};
 onMounted(() => {
   const myChart = echarts.init(chart1.value); // 初始化echarts实例
   const option = {
@@ -28,6 +43,15 @@ onMounted(() => {
   };
 
   myChart.setOption(option);
+
+  new ResizeObserver((entries) => {
+    throttle(() => {
+      myChart.resize({
+        width: entries[0].contentRect.width,
+        height: entries[0].contentRect.height,
+      });
+    }, 500)();
+  }).observe(chart1.value);
 });
 </script>
 <style lang='scss' scoped>
